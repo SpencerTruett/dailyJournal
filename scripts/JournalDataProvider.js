a68b61ab66b834c25d0f82b61596c0eb851a00cb
Spencer Truett <srtruett1993@gmail.com>
2019-11-26 13:48:04 -0600

    added js to automate entries at the bottom

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -9,21 +9,24 @@
 // This is the original data. Can't Touch This.
 const journal = [
   {
-      date: "11/12/2019",
-      concept: "Frontend Capstone Presentation: C35",
-      entry: "We watched some presentations from Cohort 35. They were interesting. Seems doable.",
-      mood: "Pretty Good"
-  ,
+    date: "11/12/2019",
+    concept: "Frontend Capstone Presentation: C35",
+    entry: "We watched some presentations from Cohort 35. They were interesting. Seems doable.",
+    mood: "Pretty Good"
+  },
+  {
     date: "11/13/2019",
     concept: "HTML",
     entry: "We discussed the basics of HTML and installed some programs.",
     mood: "Meh"
-  ,
+  },
+  {
     date: "11/14/2019",
     concept: "HTML & CSS",
     entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
     mood: "Pretty Good"
-  ,
+  },
+  {
     date: "11/15/2019",
     concept: "More HTML & CSS: Project",
     entry: "We worked on our Journals and and had 'lab time'. I sort of ran out of things to do, so I moved on to part two: this part that I'm literally typing right now. Meta.",
